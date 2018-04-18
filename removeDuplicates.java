e446e86f4c7907eee7569b007e462936f1cf20b3
cswb5511 <wangboupc@yeah.net>
2018-04-18 13:27:22 +0800

    数组内操作元素移除重复数
    给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
    
    不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
    
    维护两个指针一个是真实数据索引，一个是遍历顺序

--- removeDuplicates.java
+++ removeDuplicates.java
@@ -2,59 +2,16 @@ package com.jingdong.www;
 
 public class removeDuplicates {
 	 public int removeDuplicates(int[] nums) {
-		 	int coutn_1=0;
-		 	int coutn_2=0;
-		 	int coutn_3=0;
-		 	int coutn_4=0;
-		 	int coutn_5=0;
-		 	int coutn_6=0;
-		 	int coutn_7=0;
-		 	int coutn_8=0;
-		 	int coutn_9=0;
-		 	int coutn_0=0;
-		 	int count =0;
-		 	for (int i = 0; i < nums.length; i++) {
-				if(nums[i]==1&&coutn_1<1){
-					nums[i]=1;
-					coutn_1++;
-					count++;
-				}else if(nums[i]==2&&coutn_2<1){
-					nums[i]=2;
-					coutn_2++;
-					count++;
-				}else if(nums[i]==3&&coutn_3<1){
-					nums[i]=3;
-					coutn_3++;
-					count++;
-				}else if(nums[i]==4&&coutn_4<1){
-					nums[i]=4;
-					coutn_4++;
-					count++;
-				}else if(nums[i]==5&&coutn_5<1){
-					nums[i]=5;
-					coutn_5++;
-					count++;
-				}else if(nums[i]==6&&coutn_6<1){
-					nums[i]=6;
-					coutn_6++;
-					count++;
-				}else if(nums[i]==7&&coutn_7<1){
-					nums[i]=7;
-					coutn_7++;
-					count++;
-				}else if(nums[i]==8&&coutn_8<1){
-					nums[i]=8;coutn_8++;
-					count++;
-				}else if(nums[i]==9&&coutn_9<1){
-					nums[i]=9;
-					coutn_9++;
-					count++;
-				}else if(nums[i]==0&&coutn_0<1){
-					nums[i]=0;
-					coutn_0++;
-					count++;
+		 	if(nums.length==0||nums==null){
+		 		return 0;
+		 	}
+		 	int index=1;
+		 	for (int i = 1; i < nums.length; i++) {
+				if(nums[i]!=nums[i-1]){
+					nums[index]=nums[i];
+					index++;
 				}
 			}
-	        return count;
+		 	return index;
 	    }
 }
